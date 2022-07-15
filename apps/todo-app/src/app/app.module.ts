9aaed1ec61ae0f993a849ab22a59ad95c8629189
KMaruchsamy <kvkarti@gmail.com>
2022-07-15 15:19:47 +0530

    create new angular library

--- apps/todo-app/src/app/app.module.ts
+++ apps/todo-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { TodoLibModule } from '@nrwl-microsite/todo-lib';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    TodoLibModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
